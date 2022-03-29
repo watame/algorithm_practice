01b1067644321c6c9e1ae8bc2ab7a55913ee0ee4
watame <pandora.border@gmail.com>
2022-03-30 00:14:54 +0900

    update heap

--- ruby/heap.rb
+++ ruby/heap.rb
@@ -1,61 +1,104 @@
 class Heap
   # コンストラクタ
   def initialize(*input_numbers)
-    @numbers = *input_numbers
+    @heap_length = input_numbers.length
+    # heapを作る時は配列インデックスを1オリジンにしたいので、インスタンス変数の配列の最初に"要素数"を格納して明示的にインデックスをずらす
+    @numbers = [@heap_length, *input_numbers]
+    # ヒープ領域の初期化
+    heapify(@heap_length / 2)
   end
 
-  # ヒープ領域の初期化
-  def initialize_heap
-    # ヒープツリーが完成したかを判断するフラグ
-    retry_flg = true
-    while retry_flg
-      # 配列の半分がノードになるので、配列の前半分だけを対象にループすればよい
-      (0...@numbers.length / 2).reverse_each do |index|
-        child_node_start = (index + 1) * 2 - 1
-        child_node_end = child_node_start + 1
-        double_leaf_flg = child_node_end < @numbers.length
-
-        # 並び替え対象の配列を作成する
-        target_node = [@numbers[index], @numbers[child_node_start]]
-        target_node << @numbers[child_node_end] if double_leaf_flg
-        # 並び替え後の配列と比較するために配列をコピー
-        original_node = target_node.clone
-
-        # 配列をソート
-        sort(target_node)
-        p index
-        p child_node_start
-        p child_node_end
-
-        # ソートした値をツリーの値として配置していく
-        @numbers[index] = target_node[0]
-        @numbers[child_node_start] = target_node[1]
-        @numbers[child_node_end] = target_node[2] if double_leaf_flg
-
-        # ツリーのルートノードの並び替えを行った場合はretry_flgを有効化する
-        retry_flg = index.zero? && original_node != target_node
-      end
-    end
-  end
-
+  # 実処理
   def main
-    initialize_heap
-    p @numbers
+    # 最初の要素は管理情報なのでインデックス1から表示
+    p @numbers[1..]
   end
 
   private
 
-  def sort(target_array)
-    (0...(target_array.length - 1)).each do |current_idx|
-      next_idx = current_idx + 1
-      next unless target_array[next_idx] < target_array[current_idx]
+  # 配列を2分ヒープの構造にする
+  def heapify(loop_limit)
+    # 最上段のループにたどり着いた時点で全てがソートできているので、明示的に終了
+    return if loop_limit == 1
 
-      min = target_array[next_idx]
-      compare = target_array[current_idx]
-      target_array[current_idx] = min
-      target_array[next_idx] = compare
+    1.upto(loop_limit) do |index|
+      # 自身と子ノードを比較し、配列の中身を入れ替える
+      min_heapify(index)
+    end
+    # 処理ループ数を1減らして再度ヒープの処理を実施する
+    heapify(loop_limit - 1)
+  end
+
+  # 自分と子ノードを比較し、値を入れ替える
+  def min_heapify(current_idx)
+    # 子ノードの情報を取得
+    child_node_indexes = [current_idx * 2]
+    child_node_indexes << (current_idx * 2 + 1) unless current_idx * 2 == @heap_length
+
+    # 子ノードと自身の値を比較して入れ替えを行う
+    current_val = @numbers[current_idx]
+
+    child_node_indexes.each do |child_idx|
+      child_val = @numbers[child_idx]
+      # 現在の値よりも子ノードの値が小さいかチェックして不要な場合は入れ替えない
+      next unless child_val < current_val
+
+      # 処理対象のインデックスの値を子ノードの値と入れ替える
+      @numbers[current_idx] = child_val
+      @numbers[child_idx] = current_val
+      current_val = child_val
     end
   end
 end
 
-Heap.new(3, 3, 5, 1, 4, 5, 8, 10, 1).main
+Heap.new(3, 3, 5, 2, 4, 5, 8, 10, 1).main
+
+# 3
+# 35
+# 2458
+# 101
+#-> いれかえなし
+#
+# 3
+# 25
+# 3458
+# 101
+#-> いれかえあり
+#
+# 3
+# 25
+# 3458
+# 101
+#-> いれかえなし
+#
+# 3
+# 25
+# 1458
+# 103
+#-> いれかえあり
+#
+#
+# この段階で一番下はそろったので、3段目までループさせる
+#
+# 2
+# 35
+# 1458
+# 103
+#-> いれかえなし
+#
+# 2
+# 15
+# 3458
+# 103
+#-> いれかえあり
+#
+# この段階で3段目もそろったので、2段目までループさせる
+#
+# 1
+# 25
+# 3458
+# 103
+#-> いれかえあり
+#
+# この段階で2段目もそろったので、終了
+#
